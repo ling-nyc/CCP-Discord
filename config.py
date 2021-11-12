aef9eb414ada51a5bcddc50545634538ff4cbbdf
Ling <39093500+ling-nyc@users.noreply.github.com>
2021-11-11 20:24:04 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+import discord
+
+banned = [
+  'taiw',
+  'tiana',
+  'square',
+  'onio',
+  'winnie',
+  'pooh',
+  'onion',
+  'vpn',
+  'democracy',
+  'vote',
+  'dystopi'
+  ]
+
+propaganda = [
+  'Glory to the People\'s Democratic Party of China.',
+  'China is a great country.',
+  'Taiwan is not real.',
+  
+]
+
+blacklist = [12345678900, 123456789099] #Discord ids of people you dont want using the bot
+
+warning = "That message violates the CCP freedom of speech act. 25 social credits have been deducted."
